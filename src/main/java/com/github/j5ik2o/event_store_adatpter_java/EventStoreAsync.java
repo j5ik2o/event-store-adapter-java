37855ea0bc0e7085cca2c47ac8a7b63baf822fbd
Junichi Kato <j5ik2o@gmail.com>
2023-09-12 21:18:39 +0900

    fix: EventStore and EventStoreAsync i/f

--- src/main/java/com/github/j5ik2o/event_store_adatpter_java/EventStoreAsync.java
+++ src/main/java/com/github/j5ik2o/event_store_adatpter_java/EventStoreAsync.java
@@ -1,12 +1,31 @@
 package com.github.j5ik2o.event_store_adatpter_java;
 
+import com.github.j5ik2o.event_store_adatpter_java.internal.EventStoreAsyncForDynamoDB;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import javax.annotation.Nonnull;
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
 
 public interface EventStoreAsync<
     AID extends AggregateId, A extends Aggregate<AID>, E extends Event<AID>> {
+  static <AID extends AggregateId, A extends Aggregate<AID>, E extends Event<AID>>
+      EventStoreAsync<AID, A, E> ofDynamoDB(
+          @Nonnull DynamoDbAsyncClient dynamoDbAsyncClient,
+          @Nonnull String journalTableName,
+          @Nonnull String snapshotTableName,
+          @Nonnull String journalAidIndexName,
+          @Nonnull String snapshotAidIndexName,
+          long shardCount) {
+    return EventStoreAsyncForDynamoDB.create(
+        dynamoDbAsyncClient,
+        journalTableName,
+        snapshotTableName,
+        journalAidIndexName,
+        snapshotAidIndexName,
+        shardCount);
+  }
+
   @Nonnull
   CompletableFuture<Optional<AggregateAndVersion<AID, A>>> getLatestSnapshotById(
       @Nonnull Class<A> clazz, @Nonnull AID aggregateId);
