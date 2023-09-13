48e1bf2ee4e90caffd39f7741e9c1042a1653b81
Junichi Kato <j5ik2o@gmail.com>
2023-09-13 16:34:27 +0900

    fix: non-null annotation

--- src/main/java/com/github/j5ik2o/event/store/adapter/java/Aggregate.java
+++ src/main/java/com/github/j5ik2o/event/store/adapter/java/Aggregate.java
@@ -1,6 +1,9 @@
 package com.github.j5ik2o.event.store.adapter.java;
 
+import javax.annotation.Nonnull;
+
 public interface Aggregate<AID extends AggregateId> {
+  @Nonnull
   AID getId();
 
   long getSequenceNumber();
