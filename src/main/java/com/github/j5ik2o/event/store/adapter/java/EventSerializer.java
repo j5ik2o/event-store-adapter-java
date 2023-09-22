3de6a9148e34871598cc9ba2b88b66c328be0a85
Junichi Kato <j5ik2o@gmail.com>
2023-09-22 14:53:58 +0900

    fix: add javadocs

--- src/main/java/com/github/j5ik2o/event/store/adapter/java/EventSerializer.java
+++ src/main/java/com/github/j5ik2o/event/store/adapter/java/EventSerializer.java
@@ -2,10 +2,33 @@ package com.github.j5ik2o.event.store.adapter.java;
 
 import javax.annotation.Nonnull;
 
+/**
+ * This is an interface for serializing and deserializing events. /
+ * イベントのシリアライズとデシリアライズを行うためのインターフェース。
+ *
+ * @param <AID> Aggregate ID type / 集約IDの型
+ * @param <E> Event type / イベントの型
+ */
 public interface EventSerializer<AID extends AggregateId, E extends Event<AID>> {
+  /**
+   * Serializes the event. / イベントをシリアライズします。
+   *
+   * @param event event / イベント
+   * @return serialized event / シリアライズされたイベント
+   * @throws SerializationException if an error occurred during serialization / シリアライズ中にエラーが発生した場合
+   */
   @Nonnull
   byte[] serialize(@Nonnull E event) throws SerializationException;
 
+  /**
+   * Deserializes the event. / イベントをデシリアライズします。
+   *
+   * @param bytes bytes / バイト列
+   * @param clazz class of {@link Event} E to be deserialized / デシリアライズ対象のイベントEのクラス
+   * @return deserialized event / デシリアライズされたイベント
+   * @throws SerializationException if an error occurred during deserialization /
+   *     デシリアライズ中にエラーが発生した場合
+   */
   @Nonnull
   E deserialize(@Nonnull byte[] bytes, @Nonnull Class<E> clazz) throws SerializationException;
 }
--- src/main/java/com/github/j5ik2o/event/store/adapter/java/EventStore.java
+++ src/main/java/com/github/j5ik2o/event/store/adapter/java/EventStore.java
@@ -6,9 +6,32 @@ import java.util.Optional;
 import javax.annotation.Nonnull;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
+/**
+ * Represents an event store. / イベントストアを表します。
+ *
+ * @param <AID> AggregateId
+ * @param <A> Aggregate
+ * @param <E> Event
+ */
 public interface EventStore<
         AID extends AggregateId, A extends Aggregate<A, AID>, E extends Event<AID>>
     extends EventStoreOptions<EventStore<AID, A, E>, AID, A, E> {
+  /**
+   * Creates an instance of {@link EventStore} for DynamoDB. / DynamoDB用の{@link
+   * EventStore}のインスタンスを作成します。
+   *
+   * @param dynamoDbClient {@link DynamoDbClient}
+   * @param journalTableName journal table name / ジャーナルテーブル名
+   * @param snapshotTableName snapshot table name / スナップショットテーブル名
+   * @param journalAidIndexName journal aggregate id index name / ジャーナル集約IDインデックス名
+   * @param snapshotAidIndexName snapshot aggregate id index name / スナップショット集約IDインデックス名
+   * @param shardCount shard count / シャード数
+   * @return {@link EventStore}
+   * @param <AID> aggregate id type / 集約IDの型
+   * @param <A> aggregate type / 集約の型
+   * @param <E> event type / イベントの型
+   */
+  @Nonnull
   static <AID extends AggregateId, A extends Aggregate<A, AID>, E extends Event<AID>>
       EventStore<AID, A, E> ofDynamoDB(
           @Nonnull DynamoDbClient dynamoDbClient,
@@ -26,18 +49,59 @@ public interface EventStore<
         shardCount);
   }
 
+  /**
+   * Gets the latest snapshot by the aggregate id. / 集約IDによる最新のスナップショットを取得します。
+   *
+   * @param clazz class of {@link Aggregate} A to be serialized / シリアライズ対象の集約Aのクラス
+   * @param aggregateId　id of {@link Aggregate} A / 集約AのID
+   * @return latest snapshot / 最新のスナップショット
+   * @throws EventStoreReadException if an error occurred during reading from the event store /
+   *     イベントストアからの読み込み中にエラーが発生した場合
+   * @throws SerializationException if an error occurred during serialization / シリアライズ中にエラーが発生した場合
+   */
   @Nonnull
   Optional<A> getLatestSnapshotById(@Nonnull Class<A> clazz, @Nonnull AID aggregateId)
       throws EventStoreReadException, SerializationException;
 
+  /**
+   * Gets the events by the aggregate id and since the sequence number. / IDとシーケンス番号以降のイベントを取得します。
+   *
+   * @param clazz class of {@link Event} E to be serialized / シリアライズ対象のイベントEのクラス
+   * @param aggregateId id of {@link Aggregate} A / 集約AのID
+   * @param sequenceNumber sequence number / シーケンス番号
+   * @return events / イベント
+   * @throws EventStoreReadException if an error occurred during reading from the event store /
+   *     イベントストアからの読み込み中にエラーが発生した場合
+   * @throws SerializationException if an error occurred during serialization / シリアライズ中にエラーが発生した場合
+   */
   @Nonnull
   List<E> getEventsByIdSinceSequenceNumber(
       @Nonnull Class<E> clazz, @Nonnull AID aggregateId, long sequenceNumber)
       throws EventStoreReadException, SerializationException;
 
+  /**
+   * Persists an event only. / イベントのみを永続化します。
+   *
+   * @param event {@link Event}
+   * @param version バージョン
+   * @throws EventStoreWriteException if an error occurred during writing to the event store /
+   *     イベントストアへの書き込み中にエラーが発生した場合
+   * @throws SerializationException if an error occurred during serialization / シリアライズ中にエラーが発生した場合
+   * @throws TransactionException if an error occurred during transaction / トランザクション中にエラーが発生した場合
+   */
   void persistEvent(@Nonnull E event, long version)
       throws EventStoreWriteException, SerializationException, TransactionException;
 
+  /**
+   * Persists an event and a snapshot. / イベントとスナップショットを永続化します。
+   *
+   * @param event {@link Event}
+   * @param aggregate {@link Aggregate}
+   * @throws EventStoreWriteException if an error occurred during writing to the event store /
+   *     イベントストアへの書き込み中にエラーが発生した場合
+   * @throws SerializationException if an error occurred during serialization / シリアライズ中にエラーが発生した場合
+   * @throws TransactionException if an error occurred during transaction / トランザクション中にエラーが発生した場合
+   */
   void persistEventAndSnapshot(@Nonnull E event, @Nonnull A aggregate)
       throws EventStoreWriteException, SerializationException, TransactionException;
 }
