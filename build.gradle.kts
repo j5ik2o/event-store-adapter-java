26b5c410b911d92f81509f60744be4943cd5ba15
Junichi Kato <j5ik2o@gmail.com>
2025-04-23 19:23:11 +0900

    chore: Update group ID from com.github.j5ik2o to io.github.j5ik2o

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 
-group = "com.github.j5ik2o"
+group = "io.github.j5ik2o"
 version = File("./version").readText().trim()
 extra["isReleaseVersion"] = !version.toString().endsWith("SNAPSHOT")
 
